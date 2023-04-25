db62141f36f6902f4fbc7715629bdbc95526857e
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-04-25 17:11:11 -0300

    Menu arrumando

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import Login from './components/Login/login';
- 
+import Menu from './components/Menu/menutabs'; 
 
 export default function App() {
   const [user, setUser] = useState(null);
 
- 
-
   //verifica se existe um usuário logado, se não houver chama a
   //tela de login
+  
   if (!user) {
     return <Login changeStatus={(user) => setUser(user)} />
   }
+
+  return <Menu/>
 }
