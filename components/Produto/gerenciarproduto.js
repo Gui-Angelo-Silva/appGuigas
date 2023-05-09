bc6d8ab273702a48bbb92fb9c89d91c8337ac149
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-05-09 17:09:34 -0300

    Add dos js/produto

---
+++ components/Produto/gerenciarproduto.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react'; 
+import { 
+    View, Text, StyleSheet, 
+    TouchableOpacity, Keyboard, FlatList, ActivityIndicator 
+} from 'react-native'; 
+import { TextInput } from 'react-native-paper'; 
+
+export default function GerenciarProdutos(){
+    const [nome, setNome] = useState('');
+    const [marca, setMarca] = useState('');
+    const [preco, setPreco] = useState('');
+    const [key, setKey] = useState('');
+}
---
+++ components/Produto/listagem.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect, useRef } from 'react'; 
+import { 
+    View, Text, StyleSheet, 
+    TouchableOpacity, Keyboard, FlatList, ActivityIndicator 
+} from 'react-native'; 
+import { TextInput } from 'react-native-paper'; 
